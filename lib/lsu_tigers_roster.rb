8fe702cba61a015b51c7b84794ce77980bdbb282
Jeremy Bourne <jbourne25@yahoo.com>
2020-02-14 19:35:41 +0000

    first commit

---
+++ lib/lsu_tigers_roster.rb
@@ -0,0 +1,6 @@
+require "lsu_tigers_roster/version"
+
+module LsuTigersRoster
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/lsu_tigers_roster/version.rb
@@ -0,0 +1,3 @@
+module LsuTigersRoster
+  VERSION = "0.1.0"
+end
